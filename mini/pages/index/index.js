6b56d8f69f61041e695ef9b8b1143562b4b90e3d
hwqererere <hwqererere@gmail.com>
2019-04-13 00:40:04 +0800

    hwq

--- mini/pages/index/index.js
+++ mini/pages/index/index.js
@@ -9,7 +9,8 @@ Page({
     searchOpen:false,
     searchitems: [[[{ check: 0, txt: '决策管理类' }, { check: 0, txt: '人事行政类' }, { check: 0, txt: '财务管理' }, { check: 0, txt: '营销类' }, { check: 0, txt: '技术类' }, { check: 0, txt: '生产服务类' }, { check: 0, txt:'其他'}],[]],[
       [{ check: 0, txt: '早九晚五' }, { check: 0, txt: '双休' }, { check: 0, txt: '长白班' }, { check: 0, txt: '福利好' }, { check: 0, txt: '包吃住' }, { check: 0, txt: '班车接送' }, { check: 0, txt: '五险一金' }], [{ check: 0, txt: '长三角及扶贫专区' }, { check: 0, txt: '莘庄镇' }, { check: 0, txt: '七宝镇' }, { check: 0, txt: '大学生实践和生态环保专区' }, { check: 0, txt: '浦江镇' }, { check: 0, txt: '梅陇镇' }, { check: 0, txt: '虹桥镇' }, { check: 0, txt: '马桥镇' }, { check: 0, txt: '吴泾镇' }, { check: 0, txt: '华漕镇' }, { check: 0, txt: '颛桥镇' }, { check: 0, txt: '江川路街道' }, { check: 0, txt: '新虹街道' }, { check: 0, txt: '古美路街道' }, { check: 0, txt: '浦锦街道' } ]]],
-      allcounts:[[0,0],[0,0]]
+      allcounts:[[0,0],[0,0]],
+      job:[]
   },
   //事件处理函数
 
@@ -29,9 +30,19 @@ Page({
         self.checkresume()
       })
     }
-    
+    self.setrandomjob()
   },
-
+  setrandomjob:function(){
+    let self=this
+    let j=['信息采集员','网络管理员','仓库保管员','出纳']
+    let e=['上海智传有限公司','上海朴实有限公司','上海纳柯有限公司','上海隆基机械有限公司']
+    let jobj=[]
+    for(let i=0;i<15;i++){
+      let obj = { jobname: j[i % 4], compname: e[i % 4], area: "A区", no: ("0" + i), belong: self.data.searchitems[1][1][i].txt, minrepay: 4000, maxrepay: 8000, tag: [self.data.searchitems[1][1][i % 7].txt, self.data.searchitems[1][1][i % 7].txt, self.data.searchitems[1][1][i % 7].txt, self.data.searchitems[1][1][i % 7].txt]}
+      jobj.push(obj)
+    }
+    this.setData({ job:jobj})
+  },
   bindGetUserInfo: function (e) {
     let self = this
     if (e.detail.userInfo) {
@@ -74,9 +85,9 @@ Page({
     let idarr=id.split("-")
     let dataitem = self.data.searchitems
     let dataallcounts = self.data.allcounts
-    dataallcounts[idarr[0]][0]=0
-    dataallcounts[idarr[0]][1]=0
     if(idarr.length==1){
+      dataallcounts[idarr[0]][0] = 0
+      dataallcounts[idarr[0]][1] = 0
       for (let i = 0; i < dataitem[id][0].length; i++) {
         if (dataitem[idarr[0]][0][i].check == 1) {
           dataitem[idarr[0]][0][i].check = 0
@@ -88,13 +99,17 @@ Page({
         }
       }
     }else{
-      for (let i = 0; i < dataitem[id][1].length; i++) {
+      dataallcounts[idarr[0]][idarr[1]] = 0
+      for (let i = 0; i < dataitem[idarr[0]][idarr[1]].length; i++) {
         if (dataitem[idarr[0]][idarr[1]][i].check == 1) {
           dataitem[idarr[0]][idarr[1]][i].check = 0
         }
       }
     }    
     self.setData({ searchitems: dataitem, allcounts: dataallcounts })
+  },
+  searchitemclick:function(){
+    this.setData({ searchOpen:false})
   },
   checkresume: function () {
     /*let self=this
