93f8ecb267b8d53adb2e99338d5df276a061596c
nikola <nikola@nikoladeMacBook-Air.local>
2019-04-27 13:39:18 +0800

    h

--- minirelase/pages/index/deliverlist.js
+++ minirelase/pages/index/deliverlist.js
@@ -1,11 +1,14 @@
-// pages/index/favlist.js
+const utils = require('../../utils/util')
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    job: []
+    job: [],
+    curr:0,
+    jobtmp:[]
   },
 
   /**
@@ -20,9 +23,24 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.setData({ job: wx.getStorageSync('deli') })
+    let self = this
+    utils.requestFn('deliUserDs', { openid: wx.getStorageSync('openid') }, function (res) {
+      if (res.code == 200) {
+        self.setData({ jobtmp: res.data })
+      } else {
+        wx.showToast({
+          title: res.msg,
+          icon: "none"
+        })
+      }
 
+    })
   },
-
+  ord:function(e){
+    let curr = e.currentTarget.dataset.curr ? e.currentTarget.dataset.curr : e.target.dataset.curr
+    self.setData({curr:curr})
+    // this.
+  }
+  
   
 })
