8da9747808c83f2ed60d54733de4204523aaded3
nikola hu <hwqererere@gmail.com>
2019-05-17 16:55:26 +0800

    h

--- minirelase/pages/index2/index.js
+++ minirelase/pages/index2/index.js
@@ -1,66 +1,67 @@
-// pages/index2/index.js
+//获取应用实例
+const app = getApp()
+var route=require("../../utils/route.js")
+var model=require("../../utils/model.js")
+var utils=require("../../utils/util.js")
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    access:true,
+    addtomyminiprogram:false,    
+    pageid:"",
+    tips:"",
+    animate:"",
+    nickName:"",
+    nobgm:"0",
+    floatedData:{id:""},
+    indexData:{},
+    rankData:{},
+    getcoinData:{},
+    cashData:{},
+    cashlistData:{},
+    morepageData:{},
+    ad:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  
   onLoad: function (options) {
+    let _this=this
+    let nickName=wx.getStorageSync("nickName")?wx.getStorageSync("nickName"):''
+    _this.setData({nickName:nickName})
 
+    let pageid=options.pageid?options.pageid:""
+    if(pageid==""){
+      app.globalData.timeing=0;
+      utils.setconfigdata(options,function(){
+        route.getUrlData(_this,options,function(){
+          _this.showCallback()
+        })
+      })
+    }  
+    _this.showad()
+  },
+  onShow:function(){
+    let _this=this
+    let user_id=app.globalData.user_id?app.globalData.user_id:""
+    if(user_id==""){
+      app.globalData.user_id=wx.getStorageSync("user_id")+""
+      user_id=wx.getStorageSync("user_id")+""
+    }
+    if(user_id!=""){
+      _this.showCallback()
+    }
+    
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+ 
+  bindgetUserInfo: function(e) {
+    let _this=this
+    route.bindUserInfo(_this,e)
   },
+  fn:function(e){
+    let _this=this
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    route.fn(_this,e)
   },
+  
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
